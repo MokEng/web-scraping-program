9161fc0daab8400130daa30541ca565bc72ae89e
albinholmberg <alho1901@student.miun.se>
2022-02-21 12:40:25 +0100

    Task-classes for manipulating a webdriver.

---
+++ Web-scraping program/src/main/java/Task.java
@@ -0,0 +1,139 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Task {
+    // Data-members
+    public WebDriver webDriver;
+    public Task doFirst=null;
+    List<String> data= new ArrayList<>();
+    // Constructors
+    public Task(WebDriver driver) {
+          webDriver = driver;
+    }
+    public Task(WebDriver driver,Task doFirst) {
+        webDriver = driver;
+        this.doFirst = doFirst;
+    }
+    // Member-functions
+    abstract void execute() throws  Exception;
+    void run() throws Exception {
+        if(doFirst!=null){
+            doFirst.run();
+            data.addAll(doFirst.getData());
+            doFirst.getData().clear();
+        }
+        execute();
+    }
+    public List<String> getData() {
+        return data;
+    }
+}
+
+class TextTask extends Task {
+    private String xPathToElement;
+    String text;
+    public TextTask(WebDriver driver,String xPathToElement) {
+        super(driver);
+        this.xPathToElement = xPathToElement;
+    }
+    public TextTask(WebDriver driver, String xPathToElement, Task doFirst){
+        super(driver,doFirst);
+        this.xPathToElement = xPathToElement;
+    }
+
+    @Override
+    void execute() throws  Exception{
+        text = new WebDriverWait(webDriver,5)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xPathToElement))).getText();
+
+        data.add(text);
+    }
+
+}
+
+class ClickTask extends Task{
+    private String xPathToElement;
+    public ClickTask(WebDriver driver, String xPathToElement) {
+        super(driver);
+        this.xPathToElement = xPathToElement;
+    }
+    public ClickTask(WebDriver driver, String xPathToElement, Task doFirst){
+        super(driver, doFirst);
+        this.xPathToElement = xPathToElement;
+    }
+
+    @Override
+    void execute() throws Exception {
+        JavascriptExecutor js = (JavascriptExecutor) webDriver;
+        WebElement element = new WebDriverWait(webDriver,5)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xPathToElement)));
+        js.executeScript("arguments[0].click();",element);
+    }
+}
+
+class BackTask extends Task{
+
+    public BackTask(WebDriver driver) {
+        super(driver);
+    }
+    public BackTask(WebDriver driver, Task doFirst){
+        super(driver, doFirst);
+    }
+
+    @Override
+    void execute() throws Exception {
+        webDriver.navigate().back();
+    }
+}
+
+
+
+class NavigateTask extends Task{
+    String url;
+    public NavigateTask(WebDriver driver, String url) {
+        super(driver);
+        this.url = url;
+    }
+    public NavigateTask(WebDriver driver, String url, Task doFirst){
+        super(driver, doFirst);
+        this.url = url;
+    }
+
+    @Override
+    void execute() throws Exception {
+        webDriver.navigate().to(url);
+    }
+}
+
+class MultiLinkTask extends Task{
+    Task repeatedTask;
+    List<String> xPaths;
+    public MultiLinkTask(WebDriver driver, Task repeatedTask, List<String> xPaths) {
+        super(driver);
+        this.repeatedTask = repeatedTask;
+        this.xPaths = xPaths;
+    }
+
+    public MultiLinkTask(WebDriver driver, Task repeatedTask, List<String> xPaths, Task doFirst){
+        super(driver, doFirst);
+        this.repeatedTask = repeatedTask;
+        this.xPaths = xPaths;
+    }
+
+    @Override
+    void execute() throws Exception {
+        for(String p : xPaths){
+            new ClickTask(webDriver,p).run();
+            repeatedTask.run();
+        }
+        data.addAll(repeatedTask.getData());
+    }
+}
+
--- Web-scraping program/src/main/java/Test.java
+++ Web-scraping program/src/main/java/Test.java
@@ -3,42 +3,135 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
-    public static void main(String[] args) {
-
-        //System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+    public static void main(String[] args) throws Exception {
 
         WebDriver driver;
-
         driver = new ChromeDriver();
-        // Page for top-scorer statistics on Italian Serie A.
-        driver.navigate().to("https://www.legaseriea.it/en/serie-a/statistics");
-        // Fetch top scorers and wait for element to load. Timeout after 30 seconds.
-        WebElement topScorers=new WebDriverWait(driver,30)
-                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("body > main > div.container.pagina_wrapper.competizione > section > div:nth-child(1) > section:nth-child(1) > table > tbody")));
-        // Links to all players.
-        List<WebElement> topScorersPlayerLinks =  topScorers.findElements(By.tagName("a"));
-        System.out.println(topScorersPlayerLinks.size());
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        // Number of links available.
-        int size = topScorersPlayerLinks.size();
-        // Loop through each of the top scorers.
-        for(int x=0 ; x < size;x++ ){
-            // Fetch the elements again, otherwise error will occur because of stale elements.
-            WebElement temp=new WebDriverWait(driver,30)
-                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/main/div[1]/section/div[1]/section[1]/table/tbody")));
-            List<WebElement> playerLinks =  temp.findElements(By.tagName("a"));
-            // Execute javascript for clicking a link for more information on each player.
-            js.executeScript("arguments[0].click();",playerLinks.get(x));
-            // add info on player
-            String playerInfo1 = driver.findElement(By.xpath("/html/body/main/div[1]/section/section[1]/div[2]")).getText();
-            playerInfo1+= "\n"+driver.findElement(By.xpath("/html/body/main/div[1]/section/section[1]/div[3]")).getText();
-            System.out.println((x+1)+".\n"+playerInfo1+"\n--------------------");
-            // go back to top scorer page
-            driver.navigate().back();
+        // go to root page
+        Task nav = new NavigateTask(driver,"https://www.legaseriea.it/en/press/news");
+        nav.run();
+
+        WebElement wrapper = driver.findElement(By.xpath("/html/body/main/div[1]/section[1]"));
+        List<WebElement> elems = wrapper.findElements(By.tagName("a"));
+        // adds the paths to all links in the wrapper section of the page to a list
+        List<String> linkXpaths = elems.stream().map(e-> getAbsoluteXPath(e,driver)).toList();
+        // paths to elements in the linked page
+        String articleTitle ="/html/body/main/div[1]/section[1]/article/section/header/h1";
+        String articleContent = "/html/body/main/div[1]/section[1]/article/section/p[1]";
+
+
+        // Get title and content from all articles on page.
+        // One way to do it is with a MultiLinkTask
+
+        // Decorator pattern.
+        Task fetchArticle = new TextTask(driver,articleTitle);
+        fetchArticle = new TextTask(driver,articleContent,fetchArticle);
+        fetchArticle = new BackTask(driver,fetchArticle);
+
+        Task repeatedTask = new MultiLinkTask(driver,fetchArticle,linkXpaths);
+        try{
+            repeatedTask.run(); // run scraper
+        }catch(Exception e ){
+            System.out.println("Error while performing tasks...");
         }
+        int x = 0;
+        for(Object text : repeatedTask.getData()){
+            x++;
+            System.out.println(text);
+            if(x%2==0){
+                System.out.println("\n");
+            }
+        }
+        // Does same as above but without MultiLinkTask
+        // Can be executed on multiple threads if you use
+        // different drivers.
+        List<Task> allTasks = new ArrayList<>();
+        List<String> data = new ArrayList<>();
+        for(String p : linkXpaths){
+            Task t = new ClickTask(driver,p);
+            t = new TextTask(driver,articleTitle,t);
+            t = new TextTask(driver,articleContent,t);
+            t = new BackTask(driver,t);
+            allTasks.add(t);
+        }
+
+        for(Task task : allTasks){
+            try{
+                task.run(); // run scraper
+                System.out.println(task.getData()); // present data
+            }catch(Exception e ){
+                System.out.println("Error while performing tasks...");
+            }
+        }
+
+
+
+
         driver.close();
+    }
+
+    /*
+        Gets the absolut xPath to an element
+     */
+    public static String getAbsoluteXPath(WebElement element,WebDriver driver) {
+        return (String) ((JavascriptExecutor) driver).executeScript(
+                "function absoluteXPath(element) {"+
+                        "var comp, comps = [];"+
+                        "var parent = null;"+
+                        "var xpath = '';"+
+                        "var getPos = function(element) {"+
+                        "var position = 1, curNode;"+
+                        "if (element.nodeType == Node.ATTRIBUTE_NODE) {"+
+                        "return null;"+
+                        "}"+
+                        "for (curNode = element.previousSibling; curNode; curNode = curNode.previousSibling) {"+
+                        "if (curNode.nodeName == element.nodeName) {"+
+                        "++position;"+
+                        "}"+
+                        "}"+
+                        "return position;"+
+                        "};"+
+
+                        "if (element instanceof Document) {"+
+                        "return '/';"+
+                        "}"+
+
+                        "for (; element && !(element instanceof Document); element = element.nodeType == Node.ATTRIBUTE_NODE ? element.ownerElement : element.parentNode) {"+
+                        "comp = comps[comps.length] = {};"+
+                        "switch (element.nodeType) {"+
+                        "case Node.TEXT_NODE:"+
+                        "comp.name = 'text()';"+
+                        "break;"+
+                        "case Node.ATTRIBUTE_NODE:"+
+                        "comp.name = '@' + element.nodeName;"+
+                        "break;"+
+                        "case Node.PROCESSING_INSTRUCTION_NODE:"+
+                        "comp.name = 'processing-instruction()';"+
+                        "break;"+
+                        "case Node.COMMENT_NODE:"+
+                        "comp.name = 'comment()';"+
+                        "break;"+
+                        "case Node.ELEMENT_NODE:"+
+                        "comp.name = element.nodeName;"+
+                        "break;"+
+                        "}"+
+                        "comp.position = getPos(element);"+
+                        "}"+
+
+                        "for (var i = comps.length - 1; i >= 0; i--) {"+
+                        "comp = comps[i];"+
+                        "xpath += '/' + comp.name.toLowerCase();"+
+                        "if (comp.position !== null) {"+
+                        "xpath += '[' + comp.position + ']';"+
+                        "}"+
+                        "}"+
+
+                        "return xpath;"+
+
+                        "} return absoluteXPath(arguments[0]);", element);
     }
 }
