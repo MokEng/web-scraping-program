df8b95e6e10351a19d2ad2959d5bbab2cf0a82d7
Jonas Engberg <joen1703@student.miun.se>
2022-02-27 20:22:58 +0100

    Add element selection.

--- Web-scraping program/src/main/java/se/miun/dt002g/webscraper/gui/GUITest.java
+++ Web-scraping program/src/main/java/se/miun/dt002g/webscraper/gui/GUITest.java
@@ -3,54 +3,55 @@ package se.miun.dt002g.webscraper.gui;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.concurrent.Worker;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import org.w3c.dom.*;
+import org.w3c.dom.events.EventListener;
+import org.w3c.dom.events.EventTarget;
 import org.w3c.dom.html.HTMLElement;
-
-import java.util.*;
-
 
 public class GUITest extends Application
 {
-	private static final String CSS =
+	private static final String CSS_START =
 			".selectedNode" +
 			"{" +
-				"background-color: green !important;" +
-				"color: cyan !important;" +
-			"}";
+				"background-color: ";
+	private static final String CSS_END =
+			" !important; }";
 
-	private String selectedNodeKey = null;
-	private String selectedNodePrevClass = null;
-	private Map<String, Node> nodes;
-	private FilteredList<String> filteredList = null;
+	private Node selectedNode = null;
+	private String selectedNodePreviousClass = null;
 
 	@Override
 	public void start(Stage stage)
 	{
-		nodes = new HashMap<>();
+		//nodes = new HashMap<>();
 
 		String javaVersion = System.getProperty("java.version");
 		String javafxVersion = System.getProperty("javafx.version");
 		stage.setTitle("GUITest running on Java " + javaVersion + ", JavaFX " + javafxVersion);
 
 		Label urlLabel = new Label("URL ");
-		TextField urlField = new TextField("http://google.se");
+		TextField urlField = new TextField("https://google.se");
 		Button urlButton = new Button("Load URL");
 		Label tagLabel = new Label("HTML Tag ");
-		TextField tagField = new TextField("a");
+		TextField tagField = new TextField("*");
 
 		WebView webView = new WebView();
 
-		urlButton.setOnAction(action -> webView.getEngine().load(urlField.getText()));
+		urlButton.setOnAction(action ->
+		{
+			//if (filteredList != null) filteredList.getSource().clear();
+			webView.getEngine().load(urlField.getText());
+		});
 
 		HBox hBox = new HBox(5, urlLabel, urlField, urlButton, tagLabel, tagField);
 		HBox.setMargin(urlLabel, new Insets(5, 0, 0, 5));
@@ -61,79 +62,94 @@ public class GUITest extends Application
 		VBox.setVgrow(webView, Priority.SOMETIMES);
 		VBox.setMargin(hBox, new Insets(5, 0, 0, 0));
 
-		ListView<String> list = new ListView<>();
-		list.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
-		{
-			if (selectedNodeKey != null)
-			{
-				Node n = nodes.get(selectedNodeKey);
-				((HTMLElement)n).setClassName(selectedNodePrevClass);
-			}
+		// ---------------------------------- Selection box -----------------------------------------
+		Label idLabel = new Label("Id ");
+		TextField idField = new TextField();
+		idField.setEditable(false);
 
-			if (newValue != null)
-			{
-				selectedNodeKey = newValue.substring(newValue.indexOf(" || ")+4);
-				HTMLElement element = (HTMLElement)nodes.get(selectedNodeKey);
-				selectedNodePrevClass = element.getClassName();
-				element.setClassName(selectedNodePrevClass + " selectedNode");
-			}
-		});
+		Label typeLabel = new Label("Type ");
+		ChoiceBox<String> typeChoice = new ChoiceBox<>();
+		typeChoice.getItems().addAll("Text", "Image", "Video");
+		typeChoice.setStyle("-fx-pref-width: 100;");
 
-		Label filterLabel = new Label("Filter ");
-		TextField filterField = new TextField();
-		HBox filterHBox = new HBox(5, filterLabel, filterField);
-		HBox.setMargin(filterLabel, new Insets(10, 0, 0, 5));
-		HBox.setMargin(filterField, new Insets(5, 0, 0, 0));
-		filterField.textProperty().addListener((observable, oldValue, newValue) ->
-		{
-			String f = filterField.getText();
+		ToggleButton selectButton = new ToggleButton("Select Element");
 
-			if (f == null || f.length() == 0) filteredList.setPredicate(s -> true);
-			else filteredList.setPredicate(s -> s.contains(f));
-		});
+		Label colorLabel = new Label("Selection Color ");
+		ColorPicker colorPicker = new ColorPicker();
 
-		VBox listVBox = new VBox(5, filterHBox, list);
-		VBox.setVgrow(list, Priority.ALWAYS);
+		GridPane selectionPane = new GridPane();
+		selectionPane.add(idLabel, 0, 0);
+		selectionPane.add(idField, 1, 0);
+		selectionPane.add(typeLabel, 0, 1);
+		selectionPane.add(typeChoice, 1, 1);
+		GridPane.setHgrow(typeChoice, Priority.ALWAYS);
+		selectionPane.add(selectButton, 0, 3);
+		selectionPane.add(colorLabel, 0, 2);
+		selectionPane.add(colorPicker, 1, 2);
+		selectionPane.setVgap(10);
+		selectionPane.setHgap(10);
+		selectionPane.setStyle("-fx-border-insets: 5px; -fx-padding: 5px; -fx-background-insets: 5px");
+
+		//---------------------------------------------------------------------------------------------------
 
 		webView.getEngine().getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
 			if (newValue == Worker.State.SUCCEEDED)
 			{
 				Document doc = webView.getEngine().getDocument();
 				Element styleNode = doc.createElement("style");
-				Text styleContent = doc.createTextNode(CSS);
+				Text styleContent = doc.createTextNode(CSS_START + "#" + Integer.toHexString(colorPicker.getValue().hashCode()) + CSS_END);
 				styleNode.appendChild(styleContent);
 				doc.getDocumentElement().getElementsByTagName("head").item(0).appendChild(styleNode);
 
-				nodes.clear();
+				//nodes.clear();
 				NodeList nodeList = webView.getEngine().getDocument().getElementsByTagName(tagField.getText());
 				ObservableList<String> obsList = FXCollections.observableArrayList();
-				filteredList = new FilteredList<>(obsList);
+				//filteredList = new FilteredList<>(obsList);
 
 				for (int i = 0; i < nodeList.getLength(); i++)
 				{
 					String xPath = NodeUtilities.getXPath(nodeList.item(i));
 					//list.getItems().add(nodeList.item(i).getTextContent() + " || " + xPath);
 					obsList.add(nodeList.item(i).getTextContent() + " || " + xPath);
-					nodes.put(xPath, nodeList.item(i));
+					//nodes.put(xPath, nodeList.item(i));
+
+					addOnclick(nodeList.item(i), evt ->
+					{
+						if (selectButton.isSelected())
+						{
+							if (selectedNode != null) ((HTMLElement)selectedNode).setClassName(selectedNodePreviousClass);
+
+							selectedNode = (Node)evt.getTarget();
+							selectedNodePreviousClass = ((HTMLElement)selectedNode).getClassName();
+							((HTMLElement)selectedNode).setClassName(selectedNodePreviousClass + " selectedNode");
+
+							idField.setText(NodeUtilities.getXPath((Node)evt.getTarget()));
+							evt.preventDefault();
+							evt.stopPropagation();
+						}
+					});
 				}
 
-				list.setItems(filteredList);
+				//list.setItems(filteredList);
 			}
 			else if (newValue == Worker.State.READY)
 			{
-				filteredList.getSource().clear();
+				//filteredList.getSource().clear();
 			}
 		});
 
 		SplitPane splitPane = new SplitPane();
-		splitPane.getItems().addAll(vBox, listVBox);
+		splitPane.getItems().addAll(vBox, selectionPane);
 		Scene scene = new Scene(splitPane, 1500, 900);
 
 		stage.setScene(scene);
 		stage.show();
 	}
 
-
+	private static void addOnclick(Node node, EventListener listener)
+	{
+		((EventTarget)node).addEventListener("click", listener, true);
+	}
 
 	public static void main(String[] args)
 	{
