e2ec5ab26b8ffda70356389ea81c0cf92017cfbd
Jonas Engberg <joen1703@student.miun.se>
2022-03-12 10:39:13 +0100

    Remove unused imports

--- Web-scraping program/src/main/java/se/miun/dt002g/webscraper/gui/GUITest.java
+++ Web-scraping program/src/main/java/se/miun/dt002g/webscraper/gui/GUITest.java
@@ -3,17 +3,6 @@ package se.miun.dt002g.webscraper.gui;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import se.miun.dt002g.webscraper.scraper.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public class GUITest extends Application
 {
