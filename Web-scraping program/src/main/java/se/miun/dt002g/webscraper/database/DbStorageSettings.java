a618312495d406f122d9de042a80ddc37a37371b
albinholmberg <alho1901@student.miun.se>
2022-03-19 13:47:27 +0100

    Added documentation/comments to all classes in database package.

--- Web-scraping program/src/main/java/se/miun/dt002g/webscraper/database/DbStorageSettings.java
+++ Web-scraping program/src/main/java/se/miun/dt002g/webscraper/database/DbStorageSettings.java
@@ -2,6 +2,9 @@ package se.miun.dt002g.webscraper.database;
 
 import java.io.Serializable;
 
+/**
+ * Holds variables used for MongoDb storage
+ */
 public class DbStorageSettings implements Serializable {
     public String databaseName;
     public String collectionName;
